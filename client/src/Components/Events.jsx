d899772b82b253a9d0c15d29c8738d043ab3f6bf
Aditya Gawali <adityagawali044@gmail.com>
2024-08-24 08:18:26 +0530

    event card height adjusted for the mobile view

--- client/src/Components/Events.jsx
+++ client/src/Components/Events.jsx
@@ -40,7 +40,7 @@ const Events = () => {
 
             <div className='flex flex-col md:flex-row items-center justify-between pt-5 h-max md:h-[80vh] gap-6 md:gap-4'>
 
-                <div className='w-full h-full bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl '>
+                <div className='w-full h-[80vh] bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl '>
                     <img src="https://cdn.sanity.io/images/w8f1ak3c/production/9032e317a6741221ae543fdae5d05160819f9a44-1500x2250.jpg/DSC0079_Dexter%20Kim.jpg?rect=0,0,1500,2008&fp-x=0.47794117647058826&fp-y=0.4461323529411765&w=420&h=700&fit=crop&crop=focalpoint&auto=format"
                         alt=""
                         className='h-full w-full overflow-hidden object-cover rounded-t-xl'
@@ -50,7 +50,7 @@ const Events = () => {
                     <button className='mx-4 bg-[#27E0B3] py-2 rounded-lg px-5 text-black text-xl font-[neue]'>Register Now</button>
                 </div>
 
-                <div className='w-full h-full bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl'>
+                <div className='w-full h-[80vh] bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl'>
                     <img src="https://cdn.sanity.io/images/w8f1ak3c/production/138e191deead80c5c8e9a982c20987f8f83c1f27-5504x8256.jpg/JTG_2919%20copy.jpg?rect=275,0,4954,8256&w=420&h=700&fit=min&auto=format"
                         alt=""
                         className='h-full w-full overflow-hidden object-cover rounded-t-xl'
@@ -60,7 +60,7 @@ const Events = () => {
                     <button className='mx-4 bg-[#27E0B3] py-2 rounded-lg  px-5 text-black text-xl font-[neue]'>Register Now</button>
                 </div>
 
-                <div className='w-full h-full bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl'>
+                <div className='w-full h-[80vh] bg-[#2C2C2C] flex flex-col gap-6 pb-4 text-white rounded-xl'>
                     <img src="https://cdn.sanity.io/images/w8f1ak3c/production/d3151106849ff2494d66916cf554c68a0603444d-902x1500.png/Rectangle%20220.png?rect=1,0,900,1500&w=420&h=700&fit=min&auto=format"
                         alt=""
                         className='h-full w-full overflow-hidden object-cover rounded-t-xl'
@@ -74,5 +74,6 @@ const Events = () => {
         </div>
     )
 }
+
 
 export default Events
