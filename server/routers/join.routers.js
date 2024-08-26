12a15a275423b798f24823115d61af1f9f7fdcd1
Aditya Gawali <99984306+aditya-gawali@users.noreply.github.com>
2024-08-26 14:18:19 +0530

    Update join.routers.js

--- server/routers/join.routers.js
+++ server/routers/join.routers.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import { createJoin} from "../controllers/join.controllers.js";
-import cors from 'cors';
+
 const joinRouter = Router();
-const corsOptions = {
-    origin: 'https://itesa.onrender.com/',//(https://your-client-app.com)
-    optionsSuccessStatus: 200,
-};
 
 
-joinRouter.post('/api/join',  cors(corsOptions), createJoin);
+
+joinRouter.post('/api/join', createJoin);
 
 
 export default joinRouter;
