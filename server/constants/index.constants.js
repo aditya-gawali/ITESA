286efa5ef0af5090fa0faa63477e1c27e0f0fd22
Aditya Gawali <adityagawali044@gmail.com>
2024-09-22 23:02:39 +0530

    db user changed

--- server/constants/index.constants.js
+++ server/constants/index.constants.js
@@ -1,4 +1,4 @@
 
 export const PORT = process.env.PORT || 3000;
 
-export const DB_URL = "mongodb+srv://aditya:root123@cluster0.l32plxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export const DB_URL = "mongodb+srv://aditya:<Password>@cluster0.l32plxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
