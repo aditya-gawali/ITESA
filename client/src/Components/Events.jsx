60e5b24c3ca009e804a424a3833dbad7b9c0d53d
aditya-gawali <adityagawali044@gmail.com>
2025-01-09 20:45:25 +0530

    Event Card grid added

--- client/src/Components/Events.jsx
+++ client/src/Components/Events.jsx
@@ -4,38 +4,23 @@ import EventCard from './EventCard';
 
 const Events = () => {
 
-
-
     return (
         <div className='h-max mx-4 md:mx-14 my-20'>
             <h1 className='font-[neue] font-thin text-white text-lg md:text-3xl mb-10 leading-10 tracking-wider'>
                 Events Organized.
             </h1>
 
-            <div className='my-8 flex flex-col md:flex-row items-center justify-between pt-5 h-max md:h-[80vh] gap-6 md:gap-4'>
+
+            <div className='my-8 grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4 '>
                 <EventCard image='/images/open_mic.jpg' eventName='Open Mic.' closed={false} registerLink='https://forms.gle/jUeT9TNNanPFwkyYA' />
                 <EventCard image='/images/future_skills_in_AI.jpg' eventName='Webinar on "Future Skills in AI".' closed={false} registerLink='https://meet.google.com/kyb-tkux-fks' registerText='Join Webinar' />
                 <EventCard image='/images/poster_competition.jpg' eventName='Poster Making Contest.' />
-            </div>
-
-            <div className='flex flex-col md:flex-row items-center justify-between pt-5 h-max md:h-[80vh] gap-6 md:gap-4'>
-
                 <EventCard image='/images/roadmap.png' eventName='RoadMap Session.' />
                 <EventCard image='/images/chess.png' eventName='Chess Tournament.' />
-
                 <EventCard image='/images/Poster.jpg' eventName='SE-TE Interaction and Felicitation.' />
-
-
-            </div>
-            <div className='flex flex-col md:flex-row items-center justify-between pt-5 h-max md:h-[80vh] gap-6 md:gap-4'>
-
-
                 <EventCard image='/images/hiring.jpg' eventName='We are Recruiting.' />
-
-                <div className='w-full'></div>
-                <div className='w-full'></div>
-
             </div>
+
         </div>
     )
 }
